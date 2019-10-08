83a570044f04dbf0250b4cac4d2f09999fbfb4f6
PatrickO98 <patrick.oliveira98@live.com>
2019-10-08 14:23:21 -0300

    Adiciona modulo de busca

---
+++ clientes-web/src/app/modules/busca/busca.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+
+
+@NgModule({
+  declarations: [ResultadosComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BuscaModule { }
