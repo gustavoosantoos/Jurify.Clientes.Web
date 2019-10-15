db40af22e521a0f3d0f7ec309ea90631e98da190
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-10-14 23:43:02 -0300

    refactor: ajuste folder projeto e adição InlogMaps

---
+++ src/app/modules/busca/resultados/resultados.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as InlogMaps from '@inlog/inlog-maps/lib';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styleUrls: ['./resultados.component.scss']
+})
+export class ResultadosComponent implements OnInit {
+  private maps: InlogMaps.Map;
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    this.maps = new InlogMaps.Map();
+    this.maps.initialize(InlogMaps.MapType.Google, {
+      apiKey: 'AIzaSyCL-6vOejsS5QLc6_XI8qlvjnr6f5m6-d8',
+      gestureHandling: false
+    });
+  }
+
+  showButtons() {
+    const filterButtons = document.querySelector('.filter-buttons');
+    filterButtons.classList.toggle('show');
+  }
+
+}
